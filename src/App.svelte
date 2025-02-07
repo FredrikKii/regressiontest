ddd4cd5c2875d9001cc4a3dd967f00c0d14dce39
Julius Kaljo <julius.kaljo@hotmail.se>
2025-02-07 13:23:41 +0100

    added super basic header

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,39 @@
 <script lang="ts">
-	import Fakturering from "./lib/Fakturering.svelte";
-	import Homescreen from "./lib/Homescreen.svelte";
-	import Sidebar from "./lib/Sidebar.svelte";
-  
-	let currentScreen = "home"; // Standard: Homescreen visas först
-  
-	function setScreen(screen: string) {
-	  currentScreen = screen; // Uppdaterar aktiv skärm
-	}
-  </script>
-  
-  <main>
-	<Sidebar on:navigate={(e) => setScreen(e.detail)} />
-	
-	<!-- Dynamiskt visa rätt komponent -->
-	{#if currentScreen === "home"}
-	  <Homescreen />
-	{:else if currentScreen === "fakturering"}
-	  <Fakturering />
-	{/if}
-  </main>
-  
-  <style>
-	main {
-	  display: flex;
-	}
-  </style>
-  
+    import Fakturering from "./lib/Fakturering.svelte";
+    import Header from "./lib/Header.svelte";
+    import Homescreen from "./lib/Homescreen.svelte";
+    import Sidebar from "./lib/Sidebar.svelte";
+
+    let currentScreen = "home"; // Standard: Homescreen visas först
+
+    function setScreen(screen: string) {
+        currentScreen = screen; // Uppdaterar aktiv skärm
+    }
+</script>
+
+<main>
+    <div class="header-container">
+        <Header />
+    </div>
+
+    <Sidebar on:navigate={(e) => setScreen(e.detail)} />
+    <!-- Dynamiskt visa rätt komponent -->
+    {#if currentScreen === "home"}
+        <Homescreen />
+    {:else if currentScreen === "fakturering"}
+        <Fakturering />
+    {/if}
+</main>
+
+<style>
+    main {
+        display: flex;
+    }
+
+    .header-container {
+        position: fixed;
+        top: 10px;
+        right: 10px;
+        z-index: 100;
+    }
+</style>
---
+++ src/lib/Header.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    // Inget extra script behövs för detta exempel
+</script>
+
+<section>
+    <select name="profile">
+        <option value="account">Pelles plåt AB</option>
+        <option value="settings">Inställningar</option>
+        <option value="fishing">Fredriks Fisketips</option>
+        <option value="football">Man utd</option>
+    </select>
+    <img class="icon" src="/Profile-grey.svg" alt="profile icon" />
+    <img class="icon" src="/Help-grey.svg" alt="help icon" />
+    <img class="icon" src="/Settings-grey.svg" alt="settings icon" />
+</section>
+
+<style>
+    select {
+        /* Låt elementet anpassa sig efter innehållet */
+        width: auto;
+        /* Om du vill sätta en minsta bredd, exempelvis 10em: */
+        /* min-width: 10em; */
+        border: none;
+        height: 2em;
+        white-space: nowrap;
+    }
+
+    .icon {
+        width: 18px;
+        margin-left: 0.5em;
+    }
+</style>
